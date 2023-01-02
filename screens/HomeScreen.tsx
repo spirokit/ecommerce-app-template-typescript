686d33fdaa8253ef4ecbffc9be5c79c2f9002c49
Mauro Garcia <maurogarcia0209@gmail.com>
2023-01-02 20:18:56 -0300

    Update home screen with mocked data for ecommerce

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,97 +1,80 @@
-import { useColorModeValue, useTheme, VStack } from "@spirokit/core";
+import { StatusBar, useColorModeValue, useTheme, VStack } from "@spirokit/core";
 import React, { memo } from "react";
 import { ScrollView } from "react-native";
 import Carousel, { CarouselItem } from "../components/Carousel";
-import Header from "../components/Header";
 
 const firstCarouselItems: CarouselItem[] = [
   {
     id: 1,
-    title: "Reykjavik Food Walk - Local Foodie Adventure in Iceland",
-    badge: "Food",
-    assetUrl: "https://i.imgur.com/C1oYr4L.jpg",
+    assetUrl: "https://i.imgur.com/CpKlRgU.png",
+    alt: "Play hard t-shirt",
   },
   {
     id: 2,
-    title: "Wine Country Small-Group Tour from San Francisco with Tastings",
-    badge: "Food",
-    assetUrl: "https://i.imgur.com/50QOMLS.jpg",
+    assetUrl: "https://i.imgur.com/mM9tFLP.png",
+    alt: "Solar t-shirt",
   },
   {
     id: 3,
-    title: "Reykjavik Food Walk - Local Foodie Adventure in Iceland",
-    badge: "Food",
-    assetUrl: "https://i.imgur.com/C1oYr4L.jpg",
+    assetUrl: "https://i.imgur.com/CpKlRgU.png",
+    alt: "Play hard t-shirt",
   },
   {
     id: 4,
-    title: "Wine Country Small-Group Tour from San Francisco with Tastings",
-    badge: "Food",
-    assetUrl: "https://i.imgur.com/50QOMLS.jpg",
+    assetUrl: "https://i.imgur.com/mM9tFLP.png",
+    alt: "Solar t-shirt",
   },
 ];
 
 const secondCarouselItems: CarouselItem[] = [
   {
     id: 5,
-    title: "Bangkok Midnight Food Tour by Tuk Tuk",
-    badge: "Food",
-    assetUrl: "https://i.imgur.com/QisuDLi.jpg",
+    assetUrl: "https://i.imgur.com/qsW2nsI.png",
+    alt: "Rainbow t-shirt",
   },
   {
     id: 6,
-    title: "Private Street Food Motorbike Tour in Ho Chi Minh City",
-    badge: "Food",
-    assetUrl: "https://i.imgur.com/qUZLBVT.jpg",
+    assetUrl: "https://i.imgur.com/rSvwWy3.png",
+    alt: "PAC-MAN t-shirt",
   },
   {
     id: 7,
-    title: "Bangkok Midnight Food Tour by Tuk Tuk",
-    badge: "Food",
-    assetUrl: "https://i.imgur.com/QisuDLi.jpg",
+    assetUrl: "https://i.imgur.com/qsW2nsI.png",
+    alt: "Rainbow t-shirt",
   },
   {
     id: 8,
-    title: "Private Street Food Motorbike Tour in Ho Chi Minh City",
-    badge: "Food",
-    assetUrl: "https://i.imgur.com/qUZLBVT.jpg",
+    assetUrl: "https://i.imgur.com/rSvwWy3.png",
+    alt: "PAC-MAN t-shirt",
   },
 ];
 
 const HomeScreen = () => {
   const { colors } = useTheme();
+  const barStyle = useColorModeValue("dark-content", "light-content");
   return (
     <ScrollView
       style={{
         backgroundColor: useColorModeValue(
           colors.primaryGray["100"],
-          colors.primaryDark["1"]
+          colors.primaryDark["0"]
         ),
       }}
       contentContainerStyle={{
         flexGrow: 1,
       }}
     >
-      <Header></Header>
+      <StatusBar barStyle={barStyle}></StatusBar>
       <VStack
-        marginTop={-2}
-        borderTopRadius={16}
+        safeAreaBottom
         padding={4}
         space={4}
         flex={1}
         width="full"
-        backgroundColor={useColorModeValue("white", "primaryDark.1")}
+        backgroundColor={useColorModeValue("primaryGray.100", "primaryDark.0")}
       >
-        <Carousel
-          title="Traveler faves"
-          items={firstCarouselItems}
-          description="These top-rated experiences have all won Traveler’s Choice awards (in other words, they’re *chef kiss*)"
-        ></Carousel>
-        <Carousel
-          title="Action-packed"
-          items={secondCarouselItems}
-          description="Spice up your meal with experiences that let you do a whole lot more than order from a menu."
-        ></Carousel>
+        <Carousel title="Best Sellers" items={firstCarouselItems}></Carousel>
+        <Carousel title="New Arrivals" items={secondCarouselItems}></Carousel>
       </VStack>
     </ScrollView>
   );
