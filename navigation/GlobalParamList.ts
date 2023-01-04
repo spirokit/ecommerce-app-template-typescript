9f40a281e86587137f3b3a9e9a3b9e8c3117d551
Mauro Garcia <maurogarcia0209@gmail.com>
2023-01-03 22:05:25 -0300

    Adds checkout screen, but incomplete

--- navigation/GlobalParamList.ts
+++ navigation/GlobalParamList.ts
@@ -19,6 +19,7 @@ export type GlobalParamList = {
   VerifyAccount: {};
   More: {};
   Favorites: {};
+  Checkout: {};
 };
 
 export type ScreenNavigationProp = StackNavigationProp<GlobalParamList>;
